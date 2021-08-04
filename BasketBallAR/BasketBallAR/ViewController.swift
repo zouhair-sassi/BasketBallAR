e19bbf1ac5df2d460cf7dc0ddd269546598dba2f
zouhair sassi <zouhair.sassi@nostatikmedia.com>
2021-08-04 16:38:07 +0100

    Display the hoop on the screen

--- BasketBallAR/BasketBallAR/ViewController.swift
+++ BasketBallAR/BasketBallAR/ViewController.swift
@@ -7,18 +7,42 @@
 
 import UIKit
 import RealityKit
+import ARKit
+import SceneKit
 
-class ViewController: UIViewController {
-    
-    @IBOutlet var arView: ARView!
-    
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    //MARK: - IBOutlet
+
+    @IBOutlet var arView: ARSCNView!
+
+    //MARK: - View LifeCycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.delegate = self
+        arView.showsStatistics = true
+        let scene = SCNScene()
+        arView.scene = scene
+        addBackboard()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let config = ARWorldTrackingConfiguration()
+        arView.session.run(config, options: .resetTracking)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        arView.session.pause()
+    }
+
+    func addBackboard() {
+        guard let backboardScene = SCNScene(named: "art.scnassets/hoop.scn") else {return}
+        guard let backboardNode = backboardScene.rootNode.childNode(withName: "backboard", recursively: false) else {return}
+        backboardNode.position = SCNVector3(x: 0, y: 0.5, z: -3)
+        arView.scene.rootNode.addChildNode(backboardNode)
     }
 }
